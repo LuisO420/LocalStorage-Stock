0013431b8b91e36a20f9ef07bf4c57cc43ac43eb
LuisO420 <72115916+LuisO420@users.noreply.github.com>
2020-11-14 23:39:54 -0700

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,12 +1,23 @@
 function login() {
-    var userName = document.getElementById('uname').value;
+    var userCorreo = document.getElementById('correo').value;
     var userPass = document.getElementById('pass').value;
-    if(!localStorage.hasOwnProperty(userName)) {
-        alert("No existe la cuenta");
-    } else if(localStorage.getItem(userName) == userPass) {
-        alert("Bienvenido, " + userName);
-        location.href = 'html/vista-compra.html';
-    } else {
-        alert("La contraseña es erronea.");
-    }     
+    let lis = JSON.parse(localStorage.getItem("user"));
+
+    //buscar datos en el localstorage
+    lis.forEach(listaUser => {
+        for (var i=1;i<lis.length+1;i++){
+            if (listaUser.correo != userCorreo){
+                alert("No existe la cuenta");
+            }
+            //comparacion de datos ingresados con el localstorage
+            else if (listaUser.correo == userCorreo && listaUser.pass == userPass){
+                alert("Bienvenido, " + userCorreo);
+                location.href = 'html/vista-compra.html';
+            }
+            else {
+                alert("La contraseña es erronea.");
+            }
+        }
+    });
+
 }
