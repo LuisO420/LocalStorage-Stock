cb0d8d6d12afd50588c87da981cfa8d9d188b27b
LuisO420 <72115916+LuisO420@users.noreply.github.com>
2020-11-14 23:39:32 -0700

    Update signup.js

--- js/signup.js
+++ js/signup.js
@@ -1,8 +1,35 @@
+if (localStorage.getItem("user") === null) {
+    localStorage.setItem("user", JSON.stringify([]));
+}
+
 function signup() {     
-    var uname = document.getElementById('uname').value;    
-    var pass = document.getElementById('pass').value;    
-    localStorage.setItem(uname, pass);
-    alert("Cuenta creada éxitosamente!");   
-    document.getElementById("signup").reset();
-     
+
+//creacion del localstorage para el usuario
+if (localStorage.getItem("user") === null) {
+  localStorage.setItem("user", JSON.stringify([]));
+}
+
+//obteniendo variables para el usuario
+let usuario = JSON.parse(localStorage.getItem("user"));
+let correo = document.getElementById('correo').value;  
+let pass = document.getElementById('pass').value;    
+let nombre = document.getElementById('nom').value;    
+let apellido = document.getElementById('ape').value;    
+
+//asignar una identificacion en el localstorage a cada usuario
+let listaUser = usuario.find(lis => lis.correo == correo);
+if (listaUser === undefined) {
+    usuario.push({
+        correo: correo,
+        pass: pass,
+        nombre: nombre,
+        apellido: apellido,
+    });
+
+//guardar datos en el local storage
+localStorage.setItem("user", JSON.stringify(usuario));
+
+alert("Cuenta creada éxitosamente!");   
+document.getElementById("signup").reset();
+    }
 }
